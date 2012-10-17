260bb26fb6d9c574dbb0bfa6b261ab26f8183fc8
KACAH <kacah222@gmail.com>
2012-10-17 12:50:48 +0300

    Path independent includes

--- fetchstatic/templatetags/include_static.py
+++ fetchstatic/templatetags/include_static.py
@@ -3,51 +3,49 @@ import os
 
 from django import template
 from django.conf import settings
+from django.contrib.staticfiles.finders import get_finders
 
 
 register = template.Library()
 
-ROOT_FOLDER = settings.STATIC_LIBS["root_directory"]
-STATIC_FOLDER = os.path.join(
-    ROOT_FOLDER,
-    settings.STATIC_LIBS["fetch_directory"]
-)
-
-def get_static_name(filename):
-    """Return filename relative to static URL"""
-    
-    filename = filename.replace(ROOT_FOLDER, "")
-    if filename.startswith(os.sep):
-        filename = filename[1:]
-    return os.path.join(settings.STATIC_URL, filename)
 
 def get_js(filename):
     """Return HTML JS include by filename"""
 
     return "<script type='text/javascript' src='%s'></script>" \
-        % get_static_name(filename)
+        % os.path.join(settings.STATIC_URL, filename)
 
 def get_css(filename):
     """Return HTML CSS include by filename"""
 
     return "<link rel='stylesheet' type='text/css' href='%s'/>" \
-        % get_static_name(filename)
+        % os.path.join(settings.STATIC_URL, filename)
 
-def include_library(lib_name):
+def list_static_includes():
+    """List all known js and css static files"""
+
+    _files = []
+
+    for _finder in get_finders():
+        for (_path, _storage) in _finder.list(None):
+            if _path.endswith(".css") or _path.endswith(".js"):
+                _files.append(_path)
+    return _files
+
+def include_library(lib_name, static_files):
     """Generate list of includes for directory"""
 
     _includes = []
+    _files = [_file for _file in static_files if _file.find(lib_name) != -1]
 
-    _lib_path = os.path.join(STATIC_FOLDER, lib_name)
-    if (not os.path.exists(_lib_path)):
+    if not _files:
         _includes.append("<!-- Library '%s' not found -->" % lib_name)
         return _includes
 
-    for (_root, _dirnames, _filenames) in os.walk(_lib_path):
-        for _filename in fnmatch.filter(_filenames, "*.css"):
-            _includes.append(get_css(os.path.join(_root, _filename)))
-        for _filename in fnmatch.filter(_filenames, "*.js"):
-            _includes.append(get_js(os.path.join(_root, _filename)))
+    for _filename in fnmatch.filter(_files, "*.css"):
+        _includes.append(get_css(_filename))
+    for _filename in fnmatch.filter(_files, "*.js"):
+        _includes.append(get_js(_filename))
 
     return _includes
 
@@ -56,9 +54,10 @@ def include_static(library_name=None):
     """Include JS and CSS includes for all libraries"""
 
     _includes = []
+    _static_files = list_static_includes()
     if library_name:
-        _includes.extend(include_library(library_name))
+        _includes.extend(include_library(library_name, _static_files))
     else:
         for _lib in settings.STATIC_LIBS["libraries"]:
-            _includes.extend(include_library(_lib["name"]))
+            _includes.extend(include_library(_lib["name"], _static_files))
     return "\n".join(_includes)
